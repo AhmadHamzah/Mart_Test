782aa4de534b271a0c9a2b58e47de3e591ea4c4b
AhmadHamzah <57776420+AhmadHamzah@users.noreply.github.com>
2024-05-01 23:41:33 +0300

    f

--- js/page-logic.js
+++ js/page-logic.js
@@ -1,24 +1,36 @@
-// Open popup when clicking on the info-section
+let isPopupOpen = false;
+
+function togglePopup() {
+    let popup = document.getElementById('popup');
+    let overlay = document.getElementById('overlay');
+    if (!isPopupOpen) {
+        popup.classList.add('active');
+        overlay.classList.add('active'); // Show overlay
+        isPopupOpen = true;
+    } else {
+        popup.classList.remove('active');
+        overlay.classList.remove('active'); // Hide overlay
+        isPopupOpen = false;
+    }
+}
+
+// Open popup when clicking on either info section
 document.querySelector('.info-section').addEventListener('click', function(event) {
-    event.stopPropagation();  // Prevent this click from propagating to the body
-    document.getElementById('popup').classList.add('active');
+    event.stopPropagation(); // Prevent this click from propagating to the body
+    togglePopup();
 });
 
 document.querySelector('.info-section-two').addEventListener('click', function(event) {
-    event.stopPropagation();  // Prevent this click from propagating to the body
-    document.getElementById('popup').classList.add('active');
+    event.stopPropagation(); // Prevent this click from propagating to the body
+    togglePopup();
 });
 
-
-// Add event listener to the body to close the popup
-document.body.addEventListener('click', function(event) {
-    let popup = document.getElementById('popup');
-    if (popup.classList.contains('active')) {
-        popup.classList.remove('active');
-    }
+// Add event listener to the overlay to close the popup
+document.getElementById('overlay').addEventListener('click', function() {
+    togglePopup();
 });
 
-// Stop propagation on the popup itself to prevent the body click event from closing it when interacting with the popup
+// Stop propagation on the popup itself to prevent the overlay click event from closing it when interacting with the popup
 document.getElementById('popup').addEventListener('click', function(event) {
     event.stopPropagation();
 });
@@ -26,5 +38,5 @@ document.getElementById('popup').addEventListener('click', function(event) {
 // Close button specifically stops propagation and closes the popup
 document.querySelector('.close-btn').addEventListener('click', function(event) {
     event.stopPropagation();
-    document.getElementById('popup').classList.remove('active');
+    togglePopup();
 });
